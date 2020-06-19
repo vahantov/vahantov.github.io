7bbc98286517ff1da355fea1f2be568282367ed8
w0rtex <alen@icecode.dev>
2020-06-20 00:03:00 +0400

    Modal finished

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,76 @@ function $$(item, arr) {
     }
 }
 
+function forms() {
+    const forms = document.querySelectorAll('form')
+
+    forms.forEach(item => {
+        const inputs = item.querySelectorAll('input[required]'),
+            btn = item.querySelector('button[type="submit"]')
+
+        inputs.forEach(elem => {
+            elem.addEventListener('input', () => {
+
+                let pos = 0
+
+                for (let i = 0; i !== inputs.length; i++) {
+                    if ((inputs[i].value !== '' && inputs[i].value !== ' ') && !inputs[i].classList.contains('checkbox-required')) {
+                        pos++
+                    } else if (inputs[i].classList.contains('checkbox-required') && inputs[i].checked === true) {
+                        pos++
+                    }
+
+                    if ( i === inputs.length - 1 && pos === inputs.length ) {
+                        btn.disabled = false
+                    } else if ( i === inputs.length - 1 && pos !== inputs.length) {
+                        btn.disabled = true
+                    }
+                }
+            })
+        })
+    })
+}
+
+function modal() {
+    const btn = $$('.modal-handler', true)
+
+    btn.forEach(item => {
+        item.addEventListener('click', () => {
+            let modal = document.querySelector('#' + item.dataset.modal),
+                body = $$('body')
+
+            modal.classList.add('active')
+            body.style.overflow = 'hidden'
+
+            if ( modal.querySelector('form') ) {
+                const form = modal.querySelector('form'),
+                    formBox = modal.querySelector('.form')
+
+                form.addEventListener('submit', event =>  {
+                    event.preventDefault()
+                    /*AJAX*/
+
+                    // On success
+                    const success = modal.querySelector('.success'),
+                        descr = modal.querySelector('.descr')
+
+                    descr.querySelector('.active').classList.remove('active')
+                    descr.querySelector('.disactive').classList.add('active')
+                    descr.querySelector('.disactive').classList.remove('disactive')
+
+                    success.style.width = formBox.clientWidth + 'px'
+                    success.classList.add('active')
+                })
+            }
+
+            modal.querySelector('.close').addEventListener('click', () => {
+                modal.classList.remove('active')
+                body.removeAttribute('style')
+            })
+        })
+    })
+}
+
 function portfolioMore() {
     const box = $$('.about-reviews'),
         button = $$('.about-more a')
@@ -100,6 +170,14 @@ window.addEventListener('DOMContentLoaded', function () {
 
     if ( $$('.about-more')) {
         portfolioMore()
+    }
+
+    if ( $$('.modal')) {
+        modal()
+    }
+
+    if ( $$('form')) {
+        forms()
     }
 
 })
